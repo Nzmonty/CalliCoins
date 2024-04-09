e2a9c2bb5b8abc24b83d6eefc621777ba01fb377
Nzmonty <156123804+Nzmonty@users.noreply.github.com>
2024-04-09 15:32:11 -0400

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://it3049c-spring24.github.io/gamehub-Nzmonty/',
+  base: '/gamehub-Nzmonty/',
 })
