d215912c70cff784f56b55395a193f9e087f0198
Nzmonty <156123804+Nzmonty@users.noreply.github.com>
2024-04-11 14:56:50 -0400

    Home page update

--- src/routes/RootNav.jsx
+++ src/routes/RootNav.jsx
@@ -9,7 +9,7 @@ export default function RootNav() {
         <nav>
         <ul>
         <li>
-            <Link to="home">Home</Link>
+            <Link to="/">Home</Link>
             </li>
             <li>
             <Link to="about">About</Link>
